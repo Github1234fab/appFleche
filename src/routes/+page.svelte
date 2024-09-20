fe28bc557b472d15d9af028c23dc08b58411a2f9
Github1234fab <fabien.marceau@gmail.com>
2024-09-20 13:35:00 +0200

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,86 +7,31 @@
     import { initMessaging, initAnalytics } from "$lib/firebase";
     import { requestNotificationPermission } from "../lib/firebase.js";
 
-    // // Fonction pour gérer l'installation du nouveau service worker
-    // const handleUpdate = (installingWorker) => {
-    //     initMessaging(); // Appelle la fonction pour initialiser le service de messagerie
-    //     initAnalytics(); // Initialise Firebase Analytics si nécessaire
 
-    //     installingWorker.onstatechange = () => {
-    //         if (installingWorker.state === "installed" && navigator.serviceWorker.controller) {
-    //             // Vérifier si l'utilisateur a déjà accepté la mise à jour
-    //             const updateAccepted = localStorage.getItem('updateAccepted');
+    onMount(() => {
+        // Exécuter uniquement côté client
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/path/to/your/service-worker.js').then(registration => {
+        registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
+            installingWorker.onstatechange = () => {
+                if (installingWorker.state === 'installed') {
+                    if (navigator.serviceWorker.controller) {
+                        // Une nouvelle version est disponible
+                        console.log('Nouvelle version disponible!');
+                        // Affichez une notification ou un pop-up à l'utilisateur
+                        // Vous pouvez également envoyer un message au service worker
+                        installingWorker.postMessage({ action: 'SKIP_WAITING' });
+                    }
+                }
+            };
+        };
+    }).catch(error => {
+        console.error('Service Worker registration failed:', error);
+    });
+}
+    });
 
-    //             if (!updateAccepted) {
-    //                 const userConfirmed = confirm("Une nouvelle version est disponible. Voulez-vous l'utiliser ?");
-    //                 if (userConfirmed) {
-    //                     // Stocker l'acceptation dans localStorage
-    //                     localStorage.setItem('updateAccepted', 'true');
-    //                     // Envoyer un message au service worker pour le forcer à s'activer immédiatement
-    //                     installingWorker.postMessage({ action: "skipWaiting" });
-    //                     // Recharger la page une seule fois
-    //                     window.location.reload(true); // Ajout de 'true' pour forcer le rechargement sans cache
-    //                 }
-    //             }
-    //         }
-    //     };
-    // };
-    //     onMount(() => {
-    //     if ("serviceWorker" in navigator) {
-    //         navigator.serviceWorker.register("/onesignal-service-worker.js")
-    //             .then(() => {
-    //                 console.log("OneSignal Service Worker enregistré avec succès");
-    //                 // Initialise OneSignal ici si nécessaire
-    //                 OneSignal.push(() => {
-    //                     OneSignal.init({
-    //                         appId: "YOUR_ONESIGNAL_APP_ID",
-    //                         // Autres options de configuration
-    //                     });
-    //                 });
-    //             })
-    //             .catch((error) => {
-    //                 console.error("Échec de l'enregistrement du service worker OneSignal", error);
-    //             });
-    //     }
-    // });
-
-
-    // onMount(() => {
-    //     // Exécuter uniquement côté client
-    //     if ("serviceWorker" in navigator) {
-    //         // Enregistrer le service worker pour FCM
-    //         navigator.serviceWorker.register("/firebase-messaging-sw.js")
-    //             .then((fcmRegistration) => {
-    //                 console.log("FCM Service Worker enregistré avec succès");
-
-    //                 fcmRegistration.onupdatefound = () => {
-    //                     const installingWorker = fcmRegistration.installing;
-    //                     if (installingWorker) {
-    //                         handleUpdate(installingWorker);
-    //                     }
-    //                 };
-    //             })
-    //             .catch((err) => {
-    //                 console.error("FCM Service Worker enregistrement échoué", err);
-    //             });
-
-    //         // Enregistrer le service worker pour le cache et autres tâches
-    //         navigator.serviceWorker.register("/service-worker.js")
-    //             .then((cacheRegistration) => {
-    //                 console.log("Service Worker pour cache enregistré avec succès");
-
-    //                 cacheRegistration.onupdatefound = () => {
-    //                     const installingWorker = cacheRegistration.installing;
-    //                     if (installingWorker) {
-    //                         handleUpdate(installingWorker);
-    //                     }
-    //                 };
-    //             })
-    //             .catch((err) => {
-    //                 console.error("Service Worker pour cache enregistrement échoué", err);
-    //             });
-    //     }
-    // });
 </script>
 
 <main>
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = "flech-cache-v4";
-const urlsToCache = ["/", "/icon-192x192.png", "/icon-512x512.png", "/manifest.json"];
+// Dans votre service worker
 
 // Installer le service worker et mettre en cache les fichiers nécessaires
 self.addEventListener("install", (event) => {
@@ -10,8 +9,6 @@ self.addEventListener("install", (event) => {
                         return cache.addAll(urlsToCache);
                 })
         );
-        // Forcer l'activation du nouveau service worker immédiatement
-        self.skipWaiting();
 });
 
 // Activer le service worker et supprimer les anciens caches
@@ -29,46 +26,32 @@ self.addEventListener("activate", (event) => {
                         );
                 })
         );
-        // Forcer la prise en charge immédiate du nouveau service worker
+        // Prendre le contrôle des clients actuels
         self.clients.claim();
 });
 
-// Intercepter les requêtes et servir les fichiers à partir du cache lorsque disponible
-self.addEventListener("fetch", (event) => {
-        if (event.request.method === "GET") {
-                event.respondWith(
-                        caches.match(event.request).then((response) => {
-                                // Retourner la réponse du cache si disponible, sinon effectuer une requête réseau
-                                return (
-                                        response ||
-                                        fetch(event.request).then((fetchResponse) => {
-                                                return caches.open(CACHE_NAME).then((cache) => {
-                                                        cache.put(event.request, fetchResponse.clone());
-                                                        return fetchResponse;
-                                                });
-                                        })
-                                );
-                        })
-                );
+// Gérer les mises à jour
+self.addEventListener("message", (event) => {
+        if (event.data && event.data.action === "SKIP_WAITING") {
+                self.skipWaiting();
         }
 });
 
-// // Gérer les notifications push de Firebase Cloud Messaging (FCM)
-// self.addEventListener("push", (event) => {
-//         const data = event.data.json();
-//         console.log("Push reçu : ", data);
+// Gérer les notifications push
+self.addEventListener("push", (event) => {
+        const data = event.data.json();
+        console.log("Push reçu : ", data);
+        const options = {
+                body: data.notification.body,
+                icon: "/icon-192x192.png",
+                badge: "/badge-icon.png",
+                data: {
+                        url: data.notification.click_action || "/",
+                },
+        };
 
-//         const options = {
-//                 body: data.notification.body,
-//                 icon: "/icon-192x192.png",
-//                 badge: "/badge-icon.png",
-//                 data: {
-//                         url: data.notification.click_action || "/",
-//                 },
-//         };
-
-//         event.waitUntil(self.registration.showNotification(data.notification.title, options));
-// });
+        event.waitUntil(self.registration.showNotification(data.notification.title, options));
+});
 
 // Gérer le clic sur une notification
 self.addEventListener("notificationclick", (event) => {
@@ -86,10 +69,147 @@ self.addEventListener("notificationclick", (event) => {
                 })
         );
 });
+// Dans votre service worker
 
-// Gérer les messages pour forcer l'activation immédiate du nouveau service worker
-self.addEventListener("message", (event) => {
-        if (event.data.action === "skipWaiting") {
-                self.skipWaiting();
-        }
+// Installer le service worker et mettre en cache les fichiers nécessaires
+self.addEventListener("install", (event) => {
+    console.log("Service Worker installing.");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Cache ouvert");
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
+
+// Activer le service worker et supprimer les anciens caches
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker activating.");
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("Suppression de l'ancien cache :", cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    // Prendre le contrôle des clients actuels
+    self.clients.claim();
+});
+
+// Gérer les mises à jour
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.action === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+// Gérer les notifications push
+self.addEventListener("push", (event) => {
+    const data = event.data.json();
+    console.log("Push reçu : ", data);
+    const options = {
+        body: data.notification.body,
+        icon: "/icon-192x192.png",
+        badge: "/badge-icon.png",
+        data: {
+            url: data.notification.click_action || "/",
+        },
+    };
+
+    event.waitUntil(self.registration.showNotification(data.notification.title, options));
+});
+
+// Gérer le clic sur une notification
+self.addEventListener("notificationclick", (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+            for (const client of clientList) {
+                if (client.url === event.notification.data.url && "focus" in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
+    );
+});
+
+// Dans votre service worker
+
+// Installer le service worker et mettre en cache les fichiers nécessaires
+self.addEventListener("install", (event) => {
+    console.log("Service Worker installing.");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Cache ouvert");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activer le service worker et supprimer les anciens caches
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker activating.");
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("Suppression de l'ancien cache :", cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    // Prendre le contrôle des clients actuels
+    self.clients.claim();
+});
+
+// Gérer les mises à jour
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.action === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+// Gérer les notifications push
+self.addEventListener("push", (event) => {
+    const data = event.data.json();
+    console.log("Push reçu : ", data);
+    const options = {
+        body: data.notification.body,
+        icon: "/icon-192x192.png",
+        badge: "/badge-icon.png",
+        data: {
+            url: data.notification.click_action || "/",
+        },
+    };
+
+    event.waitUntil(self.registration.showNotification(data.notification.title, options));
+});
+
+// Gérer le clic sur une notification
+self.addEventListener("notificationclick", (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+            for (const client of clientList) {
+                if (client.url === event.notification.data.url && "focus" in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
+    );
+});
+
