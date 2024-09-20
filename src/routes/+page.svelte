0f7db68cdc72b57dd8087d3d7f262f16cde45be5
Github1234fab <fabien.marceau@gmail.com>
2024-09-20 12:33:45 +0200

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,48 +7,48 @@
     import { initMessaging, initAnalytics } from "$lib/firebase";
     import { requestNotificationPermission } from "../lib/firebase.js";
 
-    // Fonction pour gérer l'installation du nouveau service worker
-    const handleUpdate = (installingWorker) => {
-        initMessaging(); // Appelle la fonction pour initialiser le service de messagerie
-        initAnalytics(); // Initialise Firebase Analytics si nécessaire
+    // // Fonction pour gérer l'installation du nouveau service worker
+    // const handleUpdate = (installingWorker) => {
+    //     initMessaging(); // Appelle la fonction pour initialiser le service de messagerie
+    //     initAnalytics(); // Initialise Firebase Analytics si nécessaire
 
-        installingWorker.onstatechange = () => {
-            if (installingWorker.state === "installed" && navigator.serviceWorker.controller) {
-                // Vérifier si l'utilisateur a déjà accepté la mise à jour
-                const updateAccepted = localStorage.getItem('updateAccepted');
+    //     installingWorker.onstatechange = () => {
+    //         if (installingWorker.state === "installed" && navigator.serviceWorker.controller) {
+    //             // Vérifier si l'utilisateur a déjà accepté la mise à jour
+    //             const updateAccepted = localStorage.getItem('updateAccepted');
 
-                if (!updateAccepted) {
-                    const userConfirmed = confirm("Une nouvelle version est disponible. Voulez-vous l'utiliser ?");
-                    if (userConfirmed) {
-                        // Stocker l'acceptation dans localStorage
-                        localStorage.setItem('updateAccepted', 'true');
-                        // Envoyer un message au service worker pour le forcer à s'activer immédiatement
-                        installingWorker.postMessage({ action: "skipWaiting" });
-                        // Recharger la page une seule fois
-                        window.location.reload(true); // Ajout de 'true' pour forcer le rechargement sans cache
-                    }
-                }
-            }
-        };
-    };
-        onMount(() => {
-        if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("/onesignal-service-worker.js")
-                .then(() => {
-                    console.log("OneSignal Service Worker enregistré avec succès");
-                    // Initialise OneSignal ici si nécessaire
-                    OneSignal.push(() => {
-                        OneSignal.init({
-                            appId: "YOUR_ONESIGNAL_APP_ID",
-                            // Autres options de configuration
-                        });
-                    });
-                })
-                .catch((error) => {
-                    console.error("Échec de l'enregistrement du service worker OneSignal", error);
-                });
-        }
-    });
+    //             if (!updateAccepted) {
+    //                 const userConfirmed = confirm("Une nouvelle version est disponible. Voulez-vous l'utiliser ?");
+    //                 if (userConfirmed) {
+    //                     // Stocker l'acceptation dans localStorage
+    //                     localStorage.setItem('updateAccepted', 'true');
+    //                     // Envoyer un message au service worker pour le forcer à s'activer immédiatement
+    //                     installingWorker.postMessage({ action: "skipWaiting" });
+    //                     // Recharger la page une seule fois
+    //                     window.location.reload(true); // Ajout de 'true' pour forcer le rechargement sans cache
+    //                 }
+    //             }
+    //         }
+    //     };
+    // };
+    //     onMount(() => {
+    //     if ("serviceWorker" in navigator) {
+    //         navigator.serviceWorker.register("/onesignal-service-worker.js")
+    //             .then(() => {
+    //                 console.log("OneSignal Service Worker enregistré avec succès");
+    //                 // Initialise OneSignal ici si nécessaire
+    //                 OneSignal.push(() => {
+    //                     OneSignal.init({
+    //                         appId: "YOUR_ONESIGNAL_APP_ID",
+    //                         // Autres options de configuration
+    //                     });
+    //                 });
+    //             })
+    //             .catch((error) => {
+    //                 console.error("Échec de l'enregistrement du service worker OneSignal", error);
+    //             });
+    //     }
+    // });
 
 
     // onMount(() => {
