d533bbe56dee41537d6bebe4a2a92002e50b3dda
Github1234fab <fabien.marceau@gmail.com>
2024-09-20 22:36:43 +0200

    oo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,34 +3,33 @@
     import UpLoadJson from "../components/UpLoadJson.svelte";
     import UpLoadJsonAdvertisement from "../components/UpLoadJsonAdvertisement.svelte";
     import "../routes/styles.css";
-    import { onMount } from "svelte";
-    // import { initMessaging, initAnalytics } from "$lib/firebase";
-    // import { requestNotificationPermission } from "../lib/firebase.js";
+    // import { onMount } from "svelte";
+   
 
 
-    onMount(() => {
-        // Exécuter uniquement côté client
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-        registration.onupdatefound = () => {
-            const installingWorker = registration.installing;
-            installingWorker.onstatechange = () => {
-                if (installingWorker.state === 'installed') {
-                    if (navigator.serviceWorker.controller) {
-                        // Une nouvelle version est disponible
-                        console.log('Nouvelle version disponible!');
-                        // Affichez une notification ou un pop-up à l'utilisateur
-                        // Vous pouvez également envoyer un message au service worker
-                        installingWorker.postMessage({ action: 'SKIP_WAITING' });
-                    }
-                }
-            };
-        };
-    }).catch(error => {
-        console.error('Service Worker registration failed:', error);
-    });
-}
-    });
+//     onMount(() => {
+//         // Exécuter uniquement côté client
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//         registration.onupdatefound = () => {
+//             const installingWorker = registration.installing;
+//             installingWorker.onstatechange = () => {
+//                 if (installingWorker.state === 'installed') {
+//                     if (navigator.serviceWorker.controller) {
+//                         // Une nouvelle version est disponible
+//                         console.log('Nouvelle version disponible!');
+//                         // Affichez une notification ou un pop-up à l'utilisateur
+//                         // Vous pouvez également envoyer un message au service worker
+//                         installingWorker.postMessage({ action: 'SKIP_WAITING' });
+//                     }
+//                 }
+//             };
+//         };
+//     }).catch(error => {
+//         console.error('Service Worker registration failed:', error);
+//     });
+// }
+//     });
 
 </script>
 
--- static/service-worker.js
+++ static/service-worker.js
@@ -1,215 +1,215 @@
-// Dans votre service worker
+// // Dans votre service worker
 
-// Installer le service worker et mettre en cache les fichiers nécessaires
-self.addEventListener("install", (event) => {
-        console.log("Service Worker installing.");
-        event.waitUntil(
-                caches.open(CACHE_NAME).then((cache) => {
-                        console.log("Cache ouvert");
-                        return cache.addAll(urlsToCache);
-                })
-        );
-});
+// // Installer le service worker et mettre en cache les fichiers nécessaires
+// self.addEventListener("install", (event) => {
+//         console.log("Service Worker installing.");
+//         event.waitUntil(
+//                 caches.open(CACHE_NAME).then((cache) => {
+//                         console.log("Cache ouvert");
+//                         return cache.addAll(urlsToCache);
+//                 })
+//         );
+// });
 
-// Activer le service worker et supprimer les anciens caches
-self.addEventListener("activate", (event) => {
-        console.log("Service Worker activating.");
-        event.waitUntil(
-                caches.keys().then((cacheNames) => {
-                        return Promise.all(
-                                cacheNames.map((cacheName) => {
-                                        if (cacheName !== CACHE_NAME) {
-                                                console.log("Suppression de l'ancien cache :", cacheName);
-                                                return caches.delete(cacheName);
-                                        }
-                                })
-                        );
-                })
-        );
-        // Prendre le contrôle des clients actuels
-        self.clients.claim();
-});
+// // Activer le service worker et supprimer les anciens caches
+// self.addEventListener("activate", (event) => {
+//         console.log("Service Worker activating.");
+//         event.waitUntil(
+//                 caches.keys().then((cacheNames) => {
+//                         return Promise.all(
+//                                 cacheNames.map((cacheName) => {
+//                                         if (cacheName !== CACHE_NAME) {
+//                                                 console.log("Suppression de l'ancien cache :", cacheName);
+//                                                 return caches.delete(cacheName);
+//                                         }
+//                                 })
+//                         );
+//                 })
+//         );
+//         // Prendre le contrôle des clients actuels
+//         self.clients.claim();
+// });
 
-// Gérer les mises à jour
-self.addEventListener("message", (event) => {
-        if (event.data && event.data.action === "SKIP_WAITING") {
-                self.skipWaiting();
-        }
-});
+// // Gérer les mises à jour
+// self.addEventListener("message", (event) => {
+//         if (event.data && event.data.action === "SKIP_WAITING") {
+//                 self.skipWaiting();
+//         }
+// });
 
-// Gérer les notifications push
-self.addEventListener("push", (event) => {
-        const data = event.data.json();
-        console.log("Push reçu : ", data);
-        const options = {
-                body: data.notification.body,
-                icon: "/icon-192x192.png",
-                badge: "/badge-icon.png",
-                data: {
-                        url: data.notification.click_action || "/",
-                },
-        };
+// // Gérer les notifications push
+// self.addEventListener("push", (event) => {
+//         const data = event.data.json();
+//         console.log("Push reçu : ", data);
+//         const options = {
+//                 body: data.notification.body,
+//                 icon: "/icon-192x192.png",
+//                 badge: "/badge-icon.png",
+//                 data: {
+//                         url: data.notification.click_action || "/",
+//                 },
+//         };
 
-        event.waitUntil(self.registration.showNotification(data.notification.title, options));
-});
+//         event.waitUntil(self.registration.showNotification(data.notification.title, options));
+// });
 
-// Gérer le clic sur une notification
-self.addEventListener("notificationclick", (event) => {
-        event.notification.close();
-        event.waitUntil(
-                clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-                        for (const client of clientList) {
-                                if (client.url === event.notification.data.url && "focus" in client) {
-                                        return client.focus();
-                                }
-                        }
-                        if (clients.openWindow) {
-                                return clients.openWindow(event.notification.data.url);
-                        }
-                })
-        );
-});
-// Dans votre service worker
+// // Gérer le clic sur une notification
+// self.addEventListener("notificationclick", (event) => {
+//         event.notification.close();
+//         event.waitUntil(
+//                 clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+//                         for (const client of clientList) {
+//                                 if (client.url === event.notification.data.url && "focus" in client) {
+//                                         return client.focus();
+//                                 }
+//                         }
+//                         if (clients.openWindow) {
+//                                 return clients.openWindow(event.notification.data.url);
+//                         }
+//                 })
+//         );
+// });
+// // Dans votre service worker
 
-// Installer le service worker et mettre en cache les fichiers nécessaires
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.");
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("Cache ouvert");
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+// // Installer le service worker et mettre en cache les fichiers nécessaires
+// self.addEventListener("install", (event) => {
+//     console.log("Service Worker installing.");
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then((cache) => {
+//             console.log("Cache ouvert");
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
 
-// Activer le service worker et supprimer les anciens caches
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker activating.");
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log("Suppression de l'ancien cache :", cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-    // Prendre le contrôle des clients actuels
-    self.clients.claim();
-});
+// // Activer le service worker et supprimer les anciens caches
+// self.addEventListener("activate", (event) => {
+//     console.log("Service Worker activating.");
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => {
+//             return Promise.all(
+//                 cacheNames.map((cacheName) => {
+//                     if (cacheName !== CACHE_NAME) {
+//                         console.log("Suppression de l'ancien cache :", cacheName);
+//                         return caches.delete(cacheName);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+//     // Prendre le contrôle des clients actuels
+//     self.clients.claim();
+// });
 
-// Gérer les mises à jour
-self.addEventListener('message', (event) => {
-    if (event.data && event.data.action === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
-});
+// // Gérer les mises à jour
+// self.addEventListener('message', (event) => {
+//     if (event.data && event.data.action === 'SKIP_WAITING') {
+//         self.skipWaiting();
+//     }
+// });
 
-// Gérer les notifications push
-self.addEventListener("push", (event) => {
-    const data = event.data.json();
-    console.log("Push reçu : ", data);
-    const options = {
-        body: data.notification.body,
-        icon: "/icon-192x192.png",
-        badge: "/badge-icon.png",
-        data: {
-            url: data.notification.click_action || "/",
-        },
-    };
+// // Gérer les notifications push
+// self.addEventListener("push", (event) => {
+//     const data = event.data.json();
+//     console.log("Push reçu : ", data);
+//     const options = {
+//         body: data.notification.body,
+//         icon: "/icon-192x192.png",
+//         badge: "/badge-icon.png",
+//         data: {
+//             url: data.notification.click_action || "/",
+//         },
+//     };
 
-    event.waitUntil(self.registration.showNotification(data.notification.title, options));
-});
+//     event.waitUntil(self.registration.showNotification(data.notification.title, options));
+// });
 
-// Gérer le clic sur une notification
-self.addEventListener("notificationclick", (event) => {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-            for (const client of clientList) {
-                if (client.url === event.notification.data.url && "focus" in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow(event.notification.data.url);
-            }
-        })
-    );
-});
+// // Gérer le clic sur une notification
+// self.addEventListener("notificationclick", (event) => {
+//     event.notification.close();
+//     event.waitUntil(
+//         clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+//             for (const client of clientList) {
+//                 if (client.url === event.notification.data.url && "focus" in client) {
+//                     return client.focus();
+//                 }
+//             }
+//             if (clients.openWindow) {
+//                 return clients.openWindow(event.notification.data.url);
+//             }
+//         })
+//     );
+// });
 
-// Dans votre service worker
+// // Dans votre service worker
 
-// Installer le service worker et mettre en cache les fichiers nécessaires
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.");
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("Cache ouvert");
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+// // Installer le service worker et mettre en cache les fichiers nécessaires
+// self.addEventListener("install", (event) => {
+//     console.log("Service Worker installing.");
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then((cache) => {
+//             console.log("Cache ouvert");
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
 
-// Activer le service worker et supprimer les anciens caches
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker activating.");
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log("Suppression de l'ancien cache :", cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-    // Prendre le contrôle des clients actuels
-    self.clients.claim();
-});
+// // Activer le service worker et supprimer les anciens caches
+// self.addEventListener("activate", (event) => {
+//     console.log("Service Worker activating.");
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => {
+//             return Promise.all(
+//                 cacheNames.map((cacheName) => {
+//                     if (cacheName !== CACHE_NAME) {
+//                         console.log("Suppression de l'ancien cache :", cacheName);
+//                         return caches.delete(cacheName);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+//     // Prendre le contrôle des clients actuels
+//     self.clients.claim();
+// });
 
-// Gérer les mises à jour
-self.addEventListener('message', (event) => {
-    if (event.data && event.data.action === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
-});
+// // Gérer les mises à jour
+// self.addEventListener('message', (event) => {
+//     if (event.data && event.data.action === 'SKIP_WAITING') {
+//         self.skipWaiting();
+//     }
+// });
 
-// Gérer les notifications push
-self.addEventListener("push", (event) => {
-    const data = event.data.json();
-    console.log("Push reçu : ", data);
-    const options = {
-        body: data.notification.body,
-        icon: "/icon-192x192.png",
-        badge: "/badge-icon.png",
-        data: {
-            url: data.notification.click_action || "/",
-        },
-    };
+// // Gérer les notifications push
+// self.addEventListener("push", (event) => {
+//     const data = event.data.json();
+//     console.log("Push reçu : ", data);
+//     const options = {
+//         body: data.notification.body,
+//         icon: "/icon-192x192.png",
+//         badge: "/badge-icon.png",
+//         data: {
+//             url: data.notification.click_action || "/",
+//         },
+//     };
 
-    event.waitUntil(self.registration.showNotification(data.notification.title, options));
-});
+//     event.waitUntil(self.registration.showNotification(data.notification.title, options));
+// });
 
-// Gérer le clic sur une notification
-self.addEventListener("notificationclick", (event) => {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-            for (const client of clientList) {
-                if (client.url === event.notification.data.url && "focus" in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow(event.notification.data.url);
-            }
-        })
-    );
-});
+// // Gérer le clic sur une notification
+// self.addEventListener("notificationclick", (event) => {
+//     event.notification.close();
+//     event.waitUntil(
+//         clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+//             for (const client of clientList) {
+//                 if (client.url === event.notification.data.url && "focus" in client) {
+//                     return client.focus();
+//                 }
+//             }
+//             if (clients.openWindow) {
+//                 return clients.openWindow(event.notification.data.url);
+//             }
+//         })
+//     );
+// });
 
